8736a01f2b05609372eef9eaa95b8cb4a7cd8ccc
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2025-09-02 17:38:46 +0400

    fic card

--- src/pages/client/Dashboard.tsx
+++ src/pages/client/Dashboard.tsx
@@ -2,7 +2,7 @@ import { useAuthStore } from '../../store/useAuthStore';
 import { useRequestsStore } from '../../store/useRequestsStore';
 import { Badge } from '../../components/common/Badge';
 import { Link } from 'react-router-dom';
-import { Card } from '../../components/common/Card';
+import { Card } from '../../components/common/CardComponent';
 
 export function UserDashboard() {
   const currentUser = useAuthStore((s) => s.currentUser);

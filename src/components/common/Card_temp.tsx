38ef7472520620c26731765d62421523c8b3551e
Esmira Aghamedova <esmo.agamedova2020@gmail.com>
2025-09-02 17:36:50 +0400

    Temporary rename Card for case fix

---
+++ src/components/common/Card_temp.tsx
@@ -0,0 +1,13 @@
+export function Card({
+  children,
+  className = '',
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={`bg-slate-800 p-6 rounded-xl shadow ${className}`}>
+      {children}
+    </div>
+  );
+}
